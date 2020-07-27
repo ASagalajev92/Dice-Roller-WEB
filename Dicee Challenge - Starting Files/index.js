39ab802fa4cb95bdfc0af7acc32d327a35aa42ff
AleksandrS <ASagalajev92@gmail.com>
2020-07-27 15:07:05 +0300

    Created simple site for dice rolling. Using DOM and JS for rolling images.
    // TO DO 
    
    Need to add some method to multiple clicks on images.

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,39 @@
+// =================================================================================
+// FUNCTIONS
+// =================================================================================
+
+//-----   Random with min and max ranges   -----//
+function getRandom(min, max) {
+  return min + Math.floor(Math.random() * (max - min + 1));
+}
+
+//-----    Dice rolling   -----//
+function diceRolling() {
+  //variant 1
+  // image1.src = "images/dice" + randomNumber1 + ".png";
+  //variant 2
+  image1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  image2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🎉Player 1 Won 🎉";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "🎉Player 2 Won 🎉";
+  } else {
+    document.querySelector("h1").innerHTML = "🤝 It is Draw 🤝";
+  }
+}
+
+
+// =================================================================================
+// VARIABLES
+// =================================================================================
+
+//-----    Random numbers for our dice images   -----//
+var randomNumber1 = getRandom(1, 6);
+var randomNumber2 = getRandom(1, 6);
+
+var image1 = document.querySelectorAll(".img")[0];
+var image2 = document.querySelectorAll(".img")[1];
+
+document.querySelector("body").onclick = diceRolling;
